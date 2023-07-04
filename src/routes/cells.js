f64984e1ccc38ee59be0f657ae322be59c9e8796
vicente999 <vicente.delpiano@uc.cl>
2023-07-04 19:39:22 -0400

    terminado

--- src/routes/cells.js
+++ src/routes/cells.js
@@ -7,7 +7,11 @@ const router = new Router();
 //lista de las celdas de un tablero específico
 router.get('cells.show', '/:gameId', authUtils.checkUser, async (ctx) => {
     try {
-        const board = await ctx.orm.Cell.findAll({where:{gameId:ctx.params.gameId}});
+        const board = await ctx.orm.Cell.findAll({
+            where: { gameId: ctx.params.gameId },
+            order: [['row', 'ASC'], ['column', 'ASC']]
+          });
+          
         ctx.body = board;
         ctx.status = 200;
     } catch (error){

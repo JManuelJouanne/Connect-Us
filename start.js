fb84a3e1daff3232d86939e58fcce7a5115ec843
vicente999 <vicente.delpiano@uc.cl>
2023-07-04 21:11:32 -0400

    deploy

--- start.js
+++ start.js
@@ -6,6 +6,14 @@ async function deploy() {
   console.log("Installing dependencies...");
   console.log(output1);
 
+  const { stdout: output4 } = await exec("yarn db:drop");
+  console.log("Dropping database...");
+  console.log(output4);
+
+  const { stdout: output5 } = await exec("yarn db:create");
+  console.log("Creating database...");
+  console.log(output5);
+
   const { stdout: output2 } = await exec("yarn db:migrate");
   console.log("Migrating database...");
   console.log(output2);
